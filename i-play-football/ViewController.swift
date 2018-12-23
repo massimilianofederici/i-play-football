af583fdcacb7e191ec15791fb57486f59e5fe145
Massimiliano Federici <massi@massimiianosmbp.home>
2018-12-23 21:51:57 +0000

    the first commit

---
+++ i-play-football/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i-play-football
+//
+//  Created by Massimiliano Federici on 23/12/2018.
+//  Copyright © 2018 Massimiliano Federici. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ i-play-footballTests/i_play_footballTests.swift
@@ -0,0 +1,34 @@
+//
+//  i_play_footballTests.swift
+//  i-play-footballTests
+//
+//  Created by Massimiliano Federici on 23/12/2018.
+//  Copyright © 2018 Massimiliano Federici. All rights reserved.
+//
+
+import XCTest
+@testable import i_play_football
+
+class i_play_footballTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
